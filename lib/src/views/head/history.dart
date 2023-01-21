987c3a4ec50ed094a50f2eed7a80a3b96c8d8438
anttarch <ynotnaziul@gmail.com>
2023-01-20 22:57:46 -0300

    historypage: add segmented button

--- lib/src/views/head/history.dart
+++ lib/src/views/head/history.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HistoryPage extends StatelessWidget {
+enum _Date { today, week, month }
+
+class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
+
+  @override
+  State<HistoryPage> createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  Set<_Date> _date = {_Date.today};
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text(
-            'Show only:',
-            textAlign: TextAlign.start,
-            style: Theme.of(context).textTheme.labelMedium!.apply(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text(
+              'Show only:',
+              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.labelMedium!.apply(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ),
+          SegmentedButton(
+            segments: const [
+              ButtonSegment(
+                value: _Date.today,
+                label: Text('Today'),
+              ),
+              ButtonSegment(
+                value: _Date.week,
+                label: Text('Week'),
+              ),
+              ButtonSegment(
+                value: _Date.month,
+                label: Text('Month'),
+              ),
+            ],
+            selected: _date,
+            onSelectionChanged: (p0) => setState(() {
+              _date = p0;
+            }),
           ),
         ],
       ),

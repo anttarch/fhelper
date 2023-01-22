d31a70f79d12901f93b406b739a9be1a946489d3
anttarch <ynotnaziul@gmail.com>
2023-01-22 13:30:13 -0300

    historypage: add base for today, w and m views

--- lib/src/views/head/history.dart
+++ lib/src/views/head/history.dart
@@ -11,25 +11,31 @@ class HistoryPage extends StatefulWidget {
 
 class _HistoryPageState extends State<HistoryPage> {
   Set<_Date> _date = {_Date.today};
+  final PageController _pageCtrl = PageController();
 
+  final Map<_Date, int> _indexMap = {
+    _Date.today: 0,
+    _Date.week: 1,
+    _Date.month: 2
+  };
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: Text(
-              'Show only:',
-              textAlign: TextAlign.start,
-              style: Theme.of(context).textTheme.labelMedium!.apply(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+          child: Text(
+            'Show only:',
+            textAlign: TextAlign.start,
+            style: Theme.of(context).textTheme.labelMedium!.apply(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
           ),
-          SegmentedButton(
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SegmentedButton(
             segments: const [
               ButtonSegment(
                 value: _Date.today,
@@ -45,12 +51,42 @@ class _HistoryPageState extends State<HistoryPage> {
               ),
             ],
             selected: _date,
-            onSelectionChanged: (p0) => setState(() {
-              _date = p0;
-            }),
+            onSelectionChanged: (p0) {
+              setState(() {
+                _date = p0;
+              });
+              _pageCtrl.animateToPage(
+                _indexMap.entries.firstWhere((e) => e.key == p0.single).value,
+                duration: const Duration(milliseconds: 250),
+                curve: Curves.easeInOut,
+              );
+            },
           ),
-        ],
-      ),
+        ),
+        Expanded(
+          child: PageView(
+            controller: _pageCtrl,
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.red,
+              ),
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }

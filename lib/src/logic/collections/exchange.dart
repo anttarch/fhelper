1f3d68caff2b2f23f37e5af13090e01ea4b6d6b5
anttarch <ynotnaziul@gmail.com>
2023-06-05 21:31:43 -0300

    fix imports

--- lib/src/logic/collections/exchange.dart
+++ lib/src/logic/collections/exchange.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/material.dart';
-import 'package:isar/isar.dart';
-
 import 'package:fhelper/src/logic/collections/attribute.dart';
 import 'package:fhelper/src/logic/collections/card.dart' as fhelper;
 import 'package:fhelper/src/logic/collections/card_bill.dart';
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
 
 part 'exchange.g.dart';
 

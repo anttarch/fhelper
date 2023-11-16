3e2a0a7bda85d5772756fb2f6febbf5b216559fd
anttarch <ynotnaziul@gmail.com>
2023-11-16 18:36:15 -0300

    implement hidden index and cleanup

--- lib/src/widgets/listchoice.dart
+++ lib/src/widgets/listchoice.dart
@@ -47,17 +47,28 @@ class _ListChoiceState extends State<ListChoice> {
   void initState() {
     super.initState();
     if (widget.attributeMap != null) {
-      final attributeList = widget.attributeMap!.keys.toList();
+      final attributeMap = widget.attributeMap!;
+      if (widget.hiddenIndex != null) {
+        final parent = attributeMap.keys.elementAt(widget.hiddenIndex!.$1);
+        attributeMap.update(parent, (value) {
+          if (value.isNotEmpty && value.length > widget.hiddenIndex!.$2) {
+            value.removeAt(widget.hiddenIndex!.$2);
+          }
+          return value;
+        });
+      }
+
+      final parentList = attributeMap.keys.toList();
 
       _attributeParent = _AttributeParentListView(
-        parentAttributes: attributeList,
+        parentAttributes: parentList,
         onParentTap: (parentIndex) {
-          final childList = widget.attributeMap!.values.toList()[parentIndex];
+          final childList = attributeMap.values.toList()[parentIndex];
           final childView = _AttributeChildListView(
             attributeListBehavior: false,
             childAttributes: childList,
             groupValue: widget.groupValue,
-            parentName: attributeList[parentIndex].name,
+            parentName: parentList[parentIndex].name,
             parentIndex: parentIndex,
             onChanged: widget.onChanged,
           );
@@ -70,7 +81,7 @@ class _ListChoiceState extends State<ListChoice> {
 
       if (widget.attributeListBehavior) {
         _selectedChild = _AttributeChildListView(
-          childAttributes: attributeList,
+          childAttributes: parentList,
           attributeListBehavior: true,
           groupValue: widget.groupValue,
           onChanged: widget.onChanged,
@@ -122,153 +133,6 @@ class _ListChoiceState extends State<ListChoice> {
     );
   }
 }
-// @override
-// Widget build(BuildContext context) {
-//   return SafeArea(
-//     child: ListView.separated(
-//       shrinkWrap: true,
-//       padding: EdgeInsets.zero,
-//       physics: const NeverScrollableScrollPhysics(),
-//       itemCount: attributeMap != null && !attributeListBehavior
-//           ? attributeMap!.length
-//           : 1,
-//       itemBuilder: (context, parentIndex) {
-//         if (hiddenIndex != null && attributeMap != null) {
-//           final parent = attributeMap!.keys.elementAt(hiddenIndex!.$1);
-//           attributeMap!.update(parent, (value) {
-//             if (value.isNotEmpty && value.length > hiddenIndex!.$2) {
-//               value.removeAt(hiddenIndex!.$2);
-//             }
-//             return value;
-//           });
-//         }
-//         return Padding(
-//           padding: EdgeInsets.symmetric(
-//             horizontal: 22,
-//             vertical: attributeMap != null && !attributeListBehavior ? 15 : 5,
-//           ),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               if (attributeMap != null && !attributeListBehavior)
-//                 Text(
-//                   attributeMap!.keys.toList()[parentIndex].name,
-//                   style: Theme.of(context).textTheme.titleMedium,
-//                 ),
-//               Card(
-//                 elevation: 0,
-//                 margin: attributeMap != null &&
-//                         attributeMap!.values.toList()[parentIndex].isEmpty
-//                     ? null
-//                     : const EdgeInsets.only(top: 10),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(12),
-//                   side: BorderSide(
-//                     color: Theme.of(context).colorScheme.outlineVariant,
-//                   ),
-//                 ),
-//                 clipBehavior: Clip.antiAlias,
-//                 child: ListView.separated(
-//                   shrinkWrap: true,
-//                   padding: EdgeInsets.zero,
-//                   physics: const NeverScrollableScrollPhysics(),
-//                   itemCount: attributeMap != null
-//                       ? attributeListBehavior
-//                           ? attributeMap!.keys.length
-//                           : attributeMap!.values.toList()[parentIndex].length
-//                       : cardList != null
-//                           ? cardList!.length
-//                           : intList!.length,
-//                   itemBuilder: (context, childIndex) {
-//                     return RadioListTile(
-//                       value: attributeMap != null && !attributeListBehavior
-//                           ? (parentIndex, childIndex)
-//                           : childIndex,
-//                       groupValue: groupValue,
-//                       onChanged: (value) {
-//                         if (attributeMap != null) {
-//                           if (!attributeListBehavior) {
-//                             final parentName = attributeMap!.keys
-//                                 .elementAt(parentIndex)
-//                                 .name;
-//                             final childName = attributeMap!.values
-//                                 .toList()[parentIndex][childIndex]
-//                                 .name;
-//                             final name = '$parentName - $childName';
-//                             onChanged!(name, value);
-//                           } else {
-//                             onChanged!(
-//                               attributeMap!.keys.toList()[childIndex].name,
-//                               value,
-//                             );
-//                           }
-//                         } else {
-//                           onChanged!('', value);
-//                         }
-//                       },
-//                       contentPadding: const EdgeInsets.symmetric(
-//                         horizontal: 20,
-//                       ),
-//                       selected: attributeMap != null && !attributeListBehavior
-//                           ? groupValue == (parentIndex, childIndex)
-//                           : groupValue == childIndex,
-//                       selectedTileColor:
-//                           Theme.of(context).colorScheme.surfaceVariant,
-//                       shape: wid_utils.getShapeBorder(
-//                         childIndex,
-//                         attributeMap != null
-//                             ? attributeListBehavior
-//                                 ? attributeMap!.keys.length - 1
-//                                 : attributeMap!.values
-//                                         .toList()[parentIndex]
-//                                         .length -
-//                                     1
-//                             : cardList != null
-//                                 ? cardList!.length - 1
-//                                 : intList!.length - 1,
-//                       ),
-//                       title: Text(
-//                         attributeMap != null
-//                             ? attributeListBehavior
-//                                 ? attributeMap!.keys.toList()[childIndex].name
-//                                 : attributeMap!.values
-//                                     .toList()[parentIndex][childIndex]
-//                                     .name
-//                             : cardList != null
-//                                 ? cardList![childIndex].name
-//                                 : intList![childIndex].toString(),
-//                         style: Theme.of(context).textTheme.bodyLarge,
-//                       ),
-//                       controlAffinity: ListTileControlAffinity.trailing,
-//                     );
-//                   },
-//                   separatorBuilder: (_, index) {
-//                     if ((hiddenIndex != null || hiddenIndex != (-1, -1)) &&
-//                         hiddenIndex == (parentIndex, index)) {
-//                       return const SizedBox.shrink();
-//                     }
-//                     return Divider(
-//                       height: 2,
-//                       thickness: 1.5,
-//                       color: Theme.of(context).colorScheme.outlineVariant,
-//                     );
-//                   },
-//                 ),
-//               ),
-//             ],
-//           ),
-//         );
-//       },
-//       separatorBuilder: (_, __) => Divider(
-//         height: 2,
-//         thickness: 1.5,
-//         color: Theme.of(context).colorScheme.outlineVariant,
-//       ),
-//     ),
-//   );
-// }
-//}
 
 class _AttributeParentListView extends StatelessWidget {
   const _AttributeParentListView({
